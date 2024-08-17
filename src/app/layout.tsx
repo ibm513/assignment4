46d6eacb73e714efab6e54567ccf7df9367d9673
Ibrahim Khalid <ibmchemengg@gmail.com>
2024-08-17 17:15:16 +0500

    Assignment 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div className= "  flex gap-5 justify-end h-28 text-lg bg-purple-800 items-center">
+          <div className="indent-3 flex items-center h-14 bg-purple-50 rounded-full border-4 border-purple-950 opacity-80 w-44">
+            This is a header
+          </div>
+          <div className="indent-2 flex items-center h-14 bg-purple-50 rounded-full border-4 border-purple-950 opacity-80 w-44 text-nowrap">
+            This is a header 2
+          </div>
+
+        </div>
+        
+        {children}</body>
+
+
+        <footer className="bg-purple-800 py-40">
+            This is footer
+          </footer>
+        
+        
     </html>
   );
 }
